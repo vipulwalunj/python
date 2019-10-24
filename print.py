3d0255031ebb674f79b16bca90a6e418d3fbd578
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2019-10-24 21:44:05 +0530

    print function

---
+++ print.py
@@ -0,0 +1,17 @@
+#print function
+
+
+print("Hello World")    #o/p:Hello World
+#or
+print('Hello World')    #o/p:Hello World
+
+#string- collection of characters inside "" or ''
+
+
+#we can use '' inside "" and vise versa. but we can't use '' inside '' and "" inside ""
+print("hello 'world'")  #o/p:hello 'world'
+print('hello "world"')  #o/p:hello "world"
+
+#print('hello 'world'') #syntax error
+#print("hello "world"") #syntax error
+#print(hii! I'm moni')  #syntax error
