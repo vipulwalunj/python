003c7464a82dd4b37b46ca3ebca038e8ba741cd9
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2019-12-31 22:22:45 +0530

    generator comprehension

---
+++ generator_com.py
@@ -0,0 +1,24 @@
+
+#list comprehension
+square=[i**2 for i in range(1,11)]
+print(square)
+
+
+
+
+#generator comprehension
+square=(i**2 for i in range(1,11))
+print(square)
+
+
+print(next(square))
+print(next(square))
+print(next(square))
+print(next(square))
+print(next(square))
+print(next(square))
+print(next(square))
+print(next(square))
+print(next(square))
+print(next(square))
+
